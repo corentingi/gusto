e9ac76cfdd062b24b37d8a6914a2c873d3e1e5b1
Corentin Gitton <corentin.gitton@gmail.com>
2024-06-23 22:40:57 +0200

    fix: improve responsive design on mobile

--- components/page/PageContainer.vue
+++ components/page/PageContainer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="pt-5 flex flex-grow">
-    <div class="flex-grow max-w-screen-lg mx-auto rounded-t-lg bg-white border-x border-t border-stone-200 shadow-md px-10 pt-8 pb-14">
+  <div class="md:pt-5 flex flex-grow">
+    <div class="flex-grow max-w-screen-lg mx-auto md:rounded-t-lg bg-white md:border-x md:border-t md:border-stone-200 md:shadow-md px-2 py-4 md:px-10 md:pt-8 md:pb-14">
       <slot></slot>
     </div>
   </div>
--- components/page/PageNav.vue
+++ components/page/PageNav.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="h-16">
-    <nav class="fixed top-0 z-30 w-full bg-lime-50 text-amber-700 border-b border-lime-200 py-2 shadow-md shadow-black/5">
+    <nav class="fixed top-0 z-30 w-full bg-lime-50 text-amber-700 border-b border-lime-200 px-2 py-2 shadow-md shadow-black/5">
       <div class="max-w-screen-lg mx-auto flex flex-nowrap items-center justify-between gap-3">
         <div class="flex flex-nowrap gap-3 items-center">
           <NuxtLink class="flex flex-nowrap gap-3 items-center" to="/">
@@ -8,7 +8,7 @@
             <span class="text-amber-600">Gusto</span>
           </NuxtLink>
 
-          <span class="font-thin">Recettes de cuisine</span>
+          <span class="font-thin max-sm:hidden">Recettes de cuisine</span>
         </div>
 
         <div class="flex flex-nowrap gap-3 items-center">
